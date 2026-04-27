beb46c5bac76d72f3c605f95e4305cb43a59d8f9
Angel Sanchez Paredes <asp00057@red.ujaen.es>
2026-04-28 00:59:02 +0200

    Preferencias.java actualizado a spring

---
+++ src/main/java/modelos/Preferencias.java
@@ -0,0 +1,38 @@
+package modelos;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+@Component("preferencias")
+@SessionScope
+public class Preferencias implements Serializable {
+
+    private static final Logger log = Logger.getLogger(Preferencias.class.getName());
+
+    // Color por defecto (Blanco)
+    private String color = "#ffffff";
+    private String ultimoProducto = "";
+
+    public Preferencias() {
+        log.info("Bean de Preferencias inicializado.");
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // Método para aplicar el color (el botón recargará la vista y aplicará el nuevo valor)
+    public String aplicarColor() {
+        log.info("Cambiando color de fondo de la aplicación a: " + this.color);
+        return null; // Devuelve null para recargar la misma página
+    }
+
+    public String getUltimoProducto() { return ultimoProducto; }
+    public void setUltimoProducto(String ultimoProducto) { this.ultimoProducto = ultimoProducto; }
+}
